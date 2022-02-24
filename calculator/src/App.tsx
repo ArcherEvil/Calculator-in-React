a42f70abd2d4dfcfc7ca790e22e1f29063f168ae
Archer <archerevil2005@gmail.com>
2022-02-24 18:51:03 +0000

    ✅ - Done

--- calculator/src/App.tsx
+++ calculator/src/App.tsx
@@ -3,9 +3,35 @@ import Button from './button'
 import './App.css';
 
 const App = () => {
+  let Error: boolean = false
+
   const [Result, setResult] = useState('');
   const handleClick = (e: any) => {
+    if (Error === true) {
+      setResult('');
+      Error = false
+    }
     setResult(Result.concat(e.target.name));
+  }
+
+  const clear = () => {
+    setResult("");
+  }
+
+  const backspace = () => {
+    setResult(Result.slice(0, -1))
+  }
+
+  const calculate = () => {
+
+    try {
+      setResult(eval(Result).toString());
+    }
+    catch(err) {
+      setResult('Error');
+      Error = true
+    }
+
   }
     return (
     <div className='container'>
@@ -13,13 +39,13 @@ const App = () => {
             <input type='text' value={Result}/>
         </form>
         <div className="keypad">
-            <Button name='' onclick={handleClick} value='CLEAR'/>
-            <Button name='' onclick={handleClick} value='C'/>
+            <Button name='' onclick={clear} value='CLEAR'/>
+            <Button name='' onclick={backspace} value='C'/>
             <Button name='/' onclick={handleClick} value='/'/>
             <Button name='7' onclick={handleClick} value='7'/>
             <Button name='8' onclick={handleClick} value='8'/>
             <Button name='9' onclick={handleClick} value='9'/>
-            <Button name='x' onclick={handleClick} value='X'/>
+            <Button name='*' onclick={handleClick} value='X'/>
             <Button name='4' onclick={handleClick} value='4'/>
             <Button name='5' onclick={handleClick} value='5'/>
             <Button name='6' onclick={handleClick} value='6'/>
@@ -30,7 +56,7 @@ const App = () => {
             <Button name='+' onclick={handleClick} value='+'/>
             <Button name='0' onclick={handleClick} value='0'/>
             <Button name='.' onclick={handleClick} value='.'/>
-            <Button name='=' onclick={handleClick} value='='/>
+            <Button name='=' onclick={calculate} value='='/>
         </div>
     </div>
   )
